284dc0e5b1a439961e4515d4064f3299d641d644
Alessandro Carcangiu <sandro.carcangiu@gmail.com>
2015-11-08 10:10:33 +0100

    ConflictManager

---
+++ RecognitionGestureFeed_Universal/Feed/FeedBack/Conflict/ConflictManager.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Djestit
+using RecognitionGestureFeed_Universal.Djestit;
+// Handler
+using RecognitionGestureFeed_Universal.Feed.FeedBack.Tree.Wrapper.Handler;
+// Modifies
+using RecognitionGestureFeed_Universal.Feed.FeedBack.Tree.Wrapper.CustomAttributes;
+
+namespace RecognitionGestureFeed_Universal.Feed.FeedBack.Conflict
+{
+    /// Costruisce una mappa degli oggetti in conflitto sia per le gesture parzialmente riconosciute in un dato momento,
+    /// sia per tutte le funzioni associate alle varie gesture.
+    public class ConflictManager
+    {
+        /* Attributi */
+        // Elenco generale degli attributi per ogni funzione
+        public SortedDictionary<Handler, List<Modifies>> mapHandlersModifies { get; private set; }
+        // Elenco generale degli attributi per ogni funzione in conflitto
+        public SortedDictionary<Handler, List<Modifies>> mapConflicts { get; private set; }
+        // Elenco degli attributi in conflitto per le funzioni, durante l'esecuzione
+        public SortedDictionary<Handler, List<Modifies>> mapConflictExec { get; private set; }
+        // Lista degli attributi non in comune
+        private List<Modifies> listDistinct = new List<Modifies>();
+        // Lista degli attributi in conflitto
+        private List<Modifies> listConflict = new List<Modifies>();
+
+        /* Costruttore */
+        /// Attraverso l'albero delle gesture, e raccolgo per ognuno la lista di oggetti che modificano a tempo 
+        /// di esecuzione. Questi dati verranno salvati in un dizionario riportando per ogni term, con una funzione
+        /// associata al suo campo CompleteExecute, l'elenco delle variabili su cui agiscono.
+        public ConflictManager(Term term)
+        {
+            // Costruisce la map che riporta per ogni funzione la sua lista di modifies
+            visitTree(term);
+            // Costruisce la map che riporta per ogni funzione la lista di modifies in conflitto con le altre funzioni
+            determineConflict();
+        }
+
+        /* Metodi */
+        // Attraverso tutto l'albero e inserisce la coppia di elementi nella mappa
+        private void visitTree(Term term)
+        {
+            // Attraverso l'albero, quindi controllo innanzittutto se il term passato in input è
+            // un ground term o composite term
+            if (term.GetType() != typeof(GroundTerm))
+            {
+                // Casto il term in Composite Term
+                CompositeTerm exp = (CompositeTerm)term;
+                // Attraverso tutto l'albero e controllo se ogni term ha associata una funzione per 
+                // il suo completamento. Nel caso lo inserisco nel dizionario.
+                foreach (Term children in exp.children)
+                {
+                    visitTree(children);
+                    if (children.hasComplete())
+                    {
+                        mapHandlersModifies.Add(children.handler, children.handler.elementList);
+                    }
+                }
+            }
+            else
+            {
+                // È un groun term, allora non ha figli, quindi inserisco il suo contenuto nella map.
+                if (term.hasComplete())
+                {
+                    mapHandlersModifies.Add(term.handler, term.handler.elementList);
+                }
+            }
+        }
+
+        // Per ogni funzione va a determinare dove ci sono i conflitti
+        private void determineConflict()
+        {
+            // Determina tutti gli attributi non in comune
+            foreach(var elem in this.mapHandlersModifies)
+            {
+                if (listDistinct.Count > 0)
+                {
+                    // Memorizza temporaneamente il vecchio contenuto della lista
+                    List<Modifies> temp = listDistinct.ToList();
+                    // Unione con gli attributi della funzione in oggetto.
+                    listDistinct = listDistinct.Union(elem.Value).ToList();
+                    // Intersezione con la vecchia lista
+                    listConflict = listConflict.Union(temp.Intersect(elem.Value)).ToList();
+                    // Intersezione con la vecchia lista e Differenza tra le due liste
+                    listDistinct = listDistinct.Except(listConflict).ToList();
+                }
+                else
+                    listDistinct.Union(elem.Value);
+            }
+
+            // Lista temporanea di Modifies
+            List<Modifies> temps = new List<Modifies>();
+            // Quindi faccio la differenza con la lista creata in precedenza (così determino la lista degli attributi
+            // in comune).
+            foreach (var elem in this.mapHandlersModifies)
+            {
+                // Preleva la lista di attributi su cui agisce l'handler in oggetto
+                temps = elem.Value.ToList();
+                // Fa la differenza tra la lista temporanea e la lista degli attributi in conflitto
+                this.mapConflicts.Add(elem.Key, temps.Except(listDistinct).ToList());
+            }
+        }
+
+        /// <summary>
+        /// Inserisce un handler nella map.
+        /// </summary>
+        /// <param name="newHandler"></param>
+        internal void addHandler(Handler newHandler)
+        {
+            // Lista di Modifies temporanea, costruita a partire dai CustomAttributes del handler che andrà
+            // inserita nella mappa
+            List<Modifies> listModifies = new List<Modifies>(newHandler.elementList);
+
+            // Quindi per ogni handler presente nella mappa, si prende la sua lista di CustomAttribute,
+            // e si controlla se va ad agire su attributi presenti anche negli altri handler
+            foreach (var i in this.mapConflictExec)
+            {
+                // Toglie dalla lista di Modifies associato all'Handler gli eventuali elementi in conflitto
+                // ovvero quelli contenuti nell'handler della nuova gesture
+                foreach (Modifies element in i.Value.Intersect(newHandler.elementList).ToList())
+                    i.Value.Remove(element);
+
+                // Contemporaneamente aggiorna anche la lista di Modifies del nuovo handler che dovrà essere
+                // inserito
+                listModifies = listModifies.Except(i.Key.elementList).ToList();
+            }
+
+            // Inserisce l'handler nella mappa
+            this.mapConflictExec.Add(newHandler, listModifies);
+        }
+
+        /// <summary>
+        /// Rimuove il nodo dalla mappa, e aggiorna gli elementi degli eventuali handler ancora rimasti 
+        /// nella mappa.
+        /// </summary>
+        internal void removeHandler(Handler handler)
+        {
+            // Lista dei nuovi attributi che si possono inserire
+            List<Modifies> listModifies = new List<Modifies>();
+            // Liste temporanee utilizzate per determinare quali sono i nuovi attributi da inserire
+            List<Modifies> list2 = new List<Modifies>();
+
+            // Si determina quali sono i nuovi attributi in conflitto
+            foreach (var elem in this.mapConflictExec)
+            {
+                if (listModifies.Count() > 0)
+                {
+                    // Memorizza temporaneamente il vecchio contenuto della lista
+                    List<Modifies> temp = listModifies.ToList();
+                    // Unione con gli attributi della funione in oggetto.
+                    listModifies = listModifies.Union(elem.Value).ToList();
+                    // Intersezione con la vecchia lista
+                    list2 = list2.Union(temp.Intersect(elem.Value)).ToList();
+                    // Differenza tra le due liste
+                    listModifies = listModifies.Except(list2).ToList();
+                }
+                else// Inizializzazione mappa
+                {
+                    listModifies.Union(elem.Value);
+                }
+            }
+
+            /*// Prima si determina quali sono i nuovi attributi in conflitto
+            foreach (var i in this.mapHandler)
+            {
+                if (listModifies.Count > 0)
+                    listModifies.Union(i.Key.elementList);
+                else
+                    listModifies.Intersect(i.Key.elementList);
+            }*/
+
+            // Quindi si fa l'union con la lista degli elementi di partenza e poi se ne fa la differenza
+            foreach (var i in this.mapConflictExec)
+            {
+                foreach (Modifies element in i.Key.elementList.Except(listModifies).Except(i.Value).ToList())
+                {
+                    i.Value.Add(element);
+                }
+            }
+        }
+    }
+}
